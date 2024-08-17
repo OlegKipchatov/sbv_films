5e0422cc7b245e6f2af7638be27635918431e112
Oleg Kipchatov <35765711+OlegKipchatov@users.noreply.github.com>
2024-08-17 13:25:30 +0300

    ci: remove base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/sbv_films',
+    // basePath: '/sbv_films',
     images: { unoptimized: true },
 };
 
